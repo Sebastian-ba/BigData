95544b188add6bb5122d66021182da430c068c66
MGApcDev <mgapcdev@gmail.com>
2017-10-24 16:36:09 +0200

    Update test style

--- src/test/scala/travis_test.scala
+++ src/test/scala/travis_test.scala
@@ -5,19 +5,17 @@ import org.apache.spark.{SparkConf, SparkContext}
 import collection.mutable.Stack
 
 class Travis extends FunSpec with Matchers {
+    describe("A Set") {
+        describe("when empty") {
+            it("should have size 0") {
+                assert(Set.empty.size == 0)
+            }
 
-    "A Stack" should "pop values in last-in-first-out order" in {
-      val stack = new Stack[Int]
-      stack.push(1)
-      stack.push(2)
-      stack.pop() should be (2)
-      stack.pop() should be (1)
-    }
-
-    it should "throw NoSuchElementException if an empty stack is popped" in {
-      val emptyStack = new Stack[Int]
-      a [NoSuchElementException] should be thrownBy {
-        emptyStack.pop()
-      }
+            it("should produce NoSuchElementException when head is invoked") {
+                assertThrows[NoSuchElementException] {
+                    Set.empty.head
+                }
+            }
+        }
     }
 }
