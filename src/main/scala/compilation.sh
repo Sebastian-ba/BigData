dae32f711350e8d0b9209cec80704ce6b5f5d309
MGApcDev <mgapcdev@gmail.com>
2017-10-31 17:13:39 +0100

    Update filenames

--- src/main/scala/compilation.sh
+++ src/main/scala/compilation.sh
@@ -1,1 +1,1 @@
-spark-shell -i schemas.scala BatchLayer.scala BatchView1.scala BatchView2.scala BatchView3.scala
+spark-shell -i Schemas.scala BatchLayer.scala BatchView1.scala BatchView2.scala BatchView3.scala
