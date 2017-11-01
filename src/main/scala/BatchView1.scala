18abe95f4c7229f2a7981fb12803d57fb69208e8
sebab <sebab@itu.dk>
2017-11-01 08:55:40 +0100

    made a description of the goal for batchview 1

--- src/main/scala/BatchView1.scala
+++ src/main/scala/BatchView1.scala
@@ -1,4 +1,5 @@
 
+// Get a dataset that describe the connection strength for each router at each day.
 object BatchView1 {
 
 	var view:Dataset[FlattenedReadings] = Seq.empty[FlattenedReadings].toDS
