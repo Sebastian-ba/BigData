7a1bcc68e6f4d603160408f85c322190f0476881
sebab <sebab@itu.dk>
2017-11-01 09:12:07 +0100

    start with case class and import of relevant data from master dataset

--- src/main/scala/BatchView1.scala
+++ src/main/scala/BatchView1.scala
@@ -1,13 +1,31 @@
 
 // Get a dataset that describe the connection strength for each router at each day.
+
+// Case class for the output table structure.
+// Did | location | deviceFunction | deviceMode | uniformRoom | avg(rssi) | avg(snRatio) | day | ts
+case class View1(did:String, 
+		location:String, 
+		deviceFunction:String, 
+		deviceMode:String, 
+		uniformRoom:String,
+		avgRssi:Double,
+		avgSnRatio:Double,
+		day:String)
+
 object BatchView1 {
 
-	var view:Dataset[FlattenedReadings] = Seq.empty[FlattenedReadings].toDS
+	var view:Dataset[View1] = Seq.empty[View1].toDS
 
 
 	def construct():Unit = {
 		println("Constructing batch view 1...")
-		view = BatchLayer.masterDataset.devices
-		view.show()
+		
+		val devices = BatchLayer.masterDataset.devices
+		val routers = BatchLayer.masterDataset.routers
+		devices.show
+		routers.show
+
 	}
+
+
 }
